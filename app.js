4b19610ea2a5ced662217cacb4d7ee426b68933b
Dan Dorton <ddorton@xmission.com>
2018-01-13 20:43:18 -0700

    Handed auth

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
+
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('It Works!');
 });
+
+// Use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
