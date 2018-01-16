f88a656f2ada6a073b603885f41cbf581c236ee4
Dan Dorton <ddorton@xmission.com>
2018-01-15 23:41:29 -0700

    Auth Working

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ require('./config/passport')(passport);
 // Load Routes
 const auth = require('./routes/auth');
 
+// Load Keys
+const keys = require('./config/keys')
+
+// Map Global Promises
+mongoose.Promise = global.Promise;
+
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+  useMongoClient: true
+})
+  .then( () => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 const app = express();
 
--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ router.get('/google', passport.authenticate('google', {scope: ['profile', 'email
 
 router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
-  (req, res) => { res.redirect('/dashboard');
-  });
+  (req, res) => { res.redirect('/dashboard'); }
+);
 
 module.exports = router;
