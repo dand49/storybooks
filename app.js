e1181f025375d516f45463f966dbf6fc14b24b29
Dan Dorton <ddorton@xmission.com>
2018-01-19 16:06:14 -0700

    Updating Stories

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
@@ -26,7 +27,8 @@ const keys = require('./config/keys')
 const {
   truncate,
   stripTags,
-  formatDate
+  formatDate,
+  select
 } = require('./helpers/hbs');
 
 // Map Global Promises
@@ -45,12 +47,16 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Method Override Middleware
+app.use(methodOverride('_method'));
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
   helpers: {
     truncate: truncate,
     stripTags: stripTags,
-    formatDate: formatDate
+    formatDate: formatDate,
+    select: select
   },
   defaultLayout: 'main'
 }));
--- routes/stories.js
+++ routes/stories.js
@@ -34,6 +34,18 @@ router.get('/add', ensureAuthenticated, (req, res)  => {
   res.render('stories/add');
 });
 
+// Edit Story Form
+router.get('/edit/:id', ensureAuthenticated, (req, res)  => {
+  Story.findOne({
+    _id: req.params.id
+  })
+    .then(story => {
+      res.render('stories/edit', {
+        story: story
+      });
+    });
+});
+
 // Process Add Story
 router.post('/', (req, res) => {
   let allowComments;
@@ -57,6 +69,33 @@ router.post('/', (req, res) => {
     .save()
     .then(story => {
       res.redirect(`/stories/show/$(story.id)`);
+    });
+});
+
+// Edit Form Process
+router.put('/:id', (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+    .then(story => {
+      let allowComments;
+
+      if (req.body.allowComments) {
+        allowComments = true;
+      } else {
+        allowComments = false;
+      }
+
+      // New Value
+      story.title = req.body.title;
+      story.body = req.body.body;
+      story.status = req.body.status;
+      story.allowComments = allowComments;
+
+      story.save()
+        .then(story => {
+          res.redirect('/dashboard');
+        })
     });
 });
 
