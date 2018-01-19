d4ea5e53f6782495d2d7c558becc1d6e68df5ed6
Dan Dorton <ddorton@xmission.com>
2018-01-18 21:10:06 -0700

    Started Add Story

--- routes/stories.js
+++ routes/stories.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
 // Stories Index
 router.get('/',  (req, res)  => {
@@ -7,7 +8,7 @@ router.get('/',  (req, res)  => {
 });
 
 // Add Story Form
-router.get('/add',  (req, res)  => {
+router.get('/add', ensureAuthenticated, (req, res)  => {
   res.render('stories/add');
 });
 
