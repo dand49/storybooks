50800df3c9e80c5722db2107ceb6265ac00beea7
Dan Dorton <ddorton@xmission.com>
2018-01-19 23:18:03 -0700

    Alpha Candidate

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://dan:dan@ds255797.mlab.com:55797/mystory-dev',
-  googleClientID: '792918066597-fdrerpta16ipm6u4v1utvvgjcs5o271n.apps.googleusercontent.com',
-  googleClientSecret: 'Pp-fVQcUzHrU3oxL56vB7XnQ'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
