732bf18cac83f2d0f5c1bb2153eb3c7bf981c793
Dan Dorton <ddorton@xmission.com>
2018-01-19 18:52:19 -0700

    Edit Access Control

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ const {
   truncate,
   stripTags,
   formatDate,
-  select
+  select,
+  editIcon
 } = require('./helpers/hbs');
 
 // Map Global Promises
@@ -56,7 +57,8 @@ app.engine('handlebars', exphbs({
     truncate: truncate,
     stripTags: stripTags,
     formatDate: formatDate,
-    select: select
+    select: select,
+    editIcon: editIcon
   },
   defaultLayout: 'main'
 }));
