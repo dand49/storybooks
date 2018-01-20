e3908b13898fb4369bf5670410c170875b0cd83a
Dan Dorton <ddorton@xmission.com>
2018-01-20 13:56:28 -0700

    Fix Try 2

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const auth = require('./routes/auth');
 const stories = require('./routes/stories');
 
 // Load Keys
-const keys = require('./config/keys')
+const keys = require('./config/keys');
 
 // Handlebars Helpers
 const {
