1155dab30b70619564bfea8a8ea6d07d63daeac6
dand49 <dandorton@gmail.com>
2018-02-08 12:05:12 -0700

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://dan:dan@ds255797.mlab.com:55797/mystory-dev',
-  googleClientID: '792918066597-fdrerpta16ipm6u4v1utvvgjcs5o271n.apps.googleusercontent.com',
-  googleClientSecret: 'Pp-fVQcUzHrU3oxL56vB7XnQ'
-}
